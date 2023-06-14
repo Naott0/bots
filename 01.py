9d30baefe52ca3693813281e2f3ec2e9dabd63bc
Naott0 <4009695@mail.ru>
2023-06-14 18:25:43 +0300

    global 7

--- 01.py
+++ 01.py
@@ -1,6 +1,7 @@
+import time
+
 import dearpygui.dearpygui as dpg
 import pytube
-import time
 from tqdm import tqdm
 
 
@@ -17,10 +18,14 @@ class Value:
     def download(self):
         self.pytube_sop.streams.download()
         mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+        x = 0
+        dpg.set_value('bar', x)
         for i in tqdm(mylist):
-            dpg.set_value = ('bar', i)
             time.sleep(1)
+            x = + 0.1
+
+
+        print("!!!!")
 
 
 dpg.create_context()
@@ -30,7 +35,7 @@ with dpg.window(tag="Primary Window"):
     dpg.add_text("Hi! download Youtube for yourself ! ")
     dpg.add_button(label="Save", callback=value.download)
     dpg.add_input_text(label='URL', callback=value.print_value)
-    dpg.add_progress_bar(label='bar')
+    dpg.add_progress_bar(tag='bar', default_value=0)
 
 dpg.create_viewport(title='Downloader', width=400, height=100)
 dpg.setup_dearpygui()
