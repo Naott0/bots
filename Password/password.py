d343c2268802fdabed6a20c782b55f9acc30f000
александр <4009695@mail.ru>
2023-09-19 21:03:38 +0300

    add password

--- Password/password.py
+++ Password/password.py
@@ -1,12 +1,12 @@
 import random
 import os
 
-name_account = input('Введите имя аккаунта : ')
+name_account = str(input('Введите имя аккаунта : '))
 len_password = int(input('Введите длину пароля : '))
 new_password = ''.join(
     (random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_-+={}[]|') for i in
      range(len_password)))
-f = open('password.txt', 'r+')
+f = open('password.txt', 'a+')
 f.seek(0, 2)
 f.write(name_account + ': ' + new_password + '\n')
 f.close()
