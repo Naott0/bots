54d4b3bad7369a30b8dc58bbb230e342ca964c42
Naott0 <4009695@mail.ru>
2023-11-08 17:17:46 +0300

    geo

--- Geo/geo.py
+++ Geo/geo.py
@@ -1,6 +1,10 @@
 import folium
 
-# location1 = (input('введите координаты широту и долготу: '))
+location1 = (input('введите широту: '))
+location2 = (input('введите долготу: '))
 # 47.2610085, 39.6279999
-palce = folium.Map(location=[47.2610085, 39.6279999])
+print(location1)
+palce = folium.Map(location=[location1, location2])
+
 palce.save('index.html')
+print('Finish')
