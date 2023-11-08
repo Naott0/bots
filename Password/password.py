5b9db1158ad37bf3ba0732e49ed7aeb05d7466c4
Naott0 <4009695@mail.ru>
2023-11-08 17:57:54 +0300

    password edit

--- Password/password.py
+++ Password/password.py
@@ -3,12 +3,18 @@ import os
 
 name_account = str(input('Введите имя аккаунта : '))
 len_password = int(input('Введите длину пароля : '))
-new_password = ''.join(
-    (random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_-+={}[]|') for i in
-     range(len_password)))
-f = open('password.txt', 'a+')
-f.seek(0, 2)
-f.write(name_account + ': ' + new_password + '\n')
-f.close()
-print('В файл добавлен новый пароль и сохранен !!!', '\n''Путь до файла: ', os.getcwd())
-print(new_password)
+
+
+def create_password(name_account, len_password):
+    new_password = ''.join(
+        (random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_-+={}[]|') for i in
+         range(len_password)))
+    f = open('password.txt', 'a+')
+    f.seek(0, 2)
+    f.write(name_account + ': ' + new_password + '\n')
+    f.close()
+    print('В файл добавлен новый пароль и сохранен !!!', '\n''Путь до файла: ', os.getcwd())
+    print(new_password)
+
+
+create_password(name_account, len_password)
